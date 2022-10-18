f10c0844ab85211ba18814a5c159630b568fb2f0
TheLe0 <leob.tosin@hotmail.com>
2022-10-18 19:43:01 -0300

    Reduce code duplicate on About component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from '../../models/person';
 
 @Component({
   selector: 'app-about',
@@ -8,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  people: Person[];
+
+  constructor() {
+    this.people = [
+      new Person("Manuella Nevoresky", "CEO - Founder", "https://res.cloudinary.com/mhmd/image/upload/v1556834132/avatar-4_ozhrib.png"),
+      new Person("Samuel Hardy", "CEO - Founder", "https://res.cloudinary.com/mhmd/image/upload/v1556834130/avatar-3_hzlize.png"),
+      new Person("Tom Sunderland", "CEO - Founder", "https://res.cloudinary.com/mhmd/image/upload/v1556834133/avatar-2_f8dowd.png"),
+      new Person("John Tarly", "CEO - Founder", "https://res.cloudinary.com/mhmd/image/upload/v1556834133/avatar-1_s02nlg.png")
+    ]
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/models/person.ts
@@ -0,0 +1,11 @@
+export class Person {
+    name: string;
+    position: string;
+    avatar: string;
+
+    constructor(name: string, position: string, avatar: string) {
+        this.name = name;
+        this.position = position;
+        this.avatar = avatar;
+    }
+}
